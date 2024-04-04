e43930bbec58c069ddd90ca7da940c130ccdb318
ScoobertMcDuck <jamesvarnum@outlook.com>
2024-04-04 19:08:06 -0400

    Initial Commit

---
+++ Monthly Planner/Monthly_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monthly_PlannerApp.swift
+//  Monthly Planner
+//
+//  Created by Charlotte Varnum on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Monthly_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
