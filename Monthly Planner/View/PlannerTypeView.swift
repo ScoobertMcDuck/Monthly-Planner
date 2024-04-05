368a030e35628ee8f516ebb41cb9bcda66a22540
ScoobertMcDuck <jamesvarnum@outlook.com>
2024-04-05 17:37:11 -0400

    Authentication and sign out functional

--- Monthly Planner/View/PlannerTypeView.swift
+++ Monthly Planner/View/PlannerTypeView.swift
@@ -10,26 +10,43 @@ import Firebase
 
 struct PlannerTypeView: View {
     
+    @State private var userIsLoggedIn = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
-        Button {
-            try! Auth.auth().signOut()
-            
-        } label: {
-            Text("Sign Out")
-                .bold()
-                .frame(width: 200, height: 40)
-                .foregroundColor(.white)
-                .background(
-                    RoundedRectangle(cornerRadius: 10, style: .continuous)
-                        .fill(.linearGradient(colors: [.red, .red],
-                            startPoint: .topLeading, endPoint: .bottomTrailing))
-                    )
+        if userIsLoggedIn {
+          content
         }
-        .padding(.top)
+        
+        else {
+            SignUpView()
+        }
+    }
+    
+    var content: some View {
+        
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            Button {
+                try! Auth.auth().signOut()
+                userIsLoggedIn.toggle()
+                
+            } label: {
+                Text("Sign Out")
+                    .bold()
+                    .frame(width: 200, height: 40)
+                    .foregroundColor(.white)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(.linearGradient(colors: [.red, .red],
+                                                  startPoint: .topLeading, endPoint: .bottomTrailing))
+                    )
+            }
+            .padding(.top)
+        }
         
     }
+        
 }
 
 #Preview {
